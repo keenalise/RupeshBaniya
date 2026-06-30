60cc5d18acafccce92251a150c4e5520c2487d7e
Keen Alise <keenalise199@gmail.com>
2026-07-01 01:27:45 +0545

    bug fized?

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     tsconfigPaths(),
     tanstackStart(),
+    ...(command === "build" ? [nitro({ preset: "vercel" })] : []),
     react(),
     tailwindcss(),
   ],
-});
+}));
